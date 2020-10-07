9295691793b1c0012d2ab4918cc8e03f18974ad1
huuluantvka2 <huuluantvka2@gmail.com>
2020-10-07 17:51:12 +0700

    fix respon

--- js/script.js
+++ js/script.js
@@ -7,5 +7,21 @@ $(document).ready(function () {
       item.innerHTML = sliceContent;
     });
   }
-  
+  // let status_collapse = false;
+  // let change_status_collapse = document.querySelector('.navbar-toggler-icon');
+  // if (change_status_collapse) {
+  //   change_status_collapse.addEventListener('click', (e) => {
+  //     status_collapse = !status_collapse;
+  //     let background = document.querySelector('.wrap-contacts');
+  //     if (background) {
+  //       if (status_collapse) {
+  //         background.classList.add('hidden-wrap');
+  //       } else {
+  //         setTimeout(() => {
+  //           background.classList.remove('hidden-wrap');
+  //         }, 200);
+  //       }
+  //     }
+  //   });
+  // }
 });
